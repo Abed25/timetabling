8f36935ca3e685a488042907c00a4dc04a03c715
Abed25 <officialwekesa254@gmail.com>
2025-02-14 23:58:14 +0300

    update on routes

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -3,10 +3,11 @@ import cors from "cors";
 import routes from "./routes/index.mjs";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
